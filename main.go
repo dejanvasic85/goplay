0bf86c6e460afbff99b655f2085e192417ed028b
Dejan Vasic <dejanvasic24@gmail.com>
2019-11-01 22:45:39 +1100

    Adding correlation ID middleware

--- main.go
+++ main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
 
-var users = map[string]string {
+var users = map[string]string{
 	"foo": "Mister Fooooo",
 	"bar": "Missus Barrrr",
 }
 
+func CorrelationMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := r.Header.Get("Correlation-ID")
+		ctx := context.WithValue(r.Context(), "CorrelationID", id)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func HandleGetUser(w http.ResponseWriter, r *http.Request) {
-	userID := r.Form.Get("id")
+	params := mux.Vars(r)
+	userID := params["id"]
+
 	log.Infof("Fetching user %s", userID)
 
-	if value, exists := users.Exists
+	if value, exists := users[userID]; exists {
+		log.Infof("Found user %s", value)
+		data := struct {
+			Hello string
+		}{
+			Hello: value,
+		}
 
-	w.Write([]byte("Ok"))
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(data)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func main() {
 	r := mux.NewRouter()
 
+	r.Use(CorrelationMiddleware)
 	r.HandleFunc("/users/{id}", HandleGetUser)
 
 	log.Info("Starting server on port 9001")
